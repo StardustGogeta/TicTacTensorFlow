bfe669d5c91682e0bff14cec41a5d1eca7e12d52
StardustGogeta <liam.gerst@gmail.com>
2021-03-07 19:23:49 -0500

    Making progress on neural net

--- game.py
+++ game.py
@@ -1,1 +1,81 @@
+import numpy as np
+import copy
 
+class Game:
+    def startState():
+        # returns state (3x3 array)
+        # 0 is blank
+        # 1 is X
+        # -1 is O
+        return np.zeros((3,3))
+
+    def nextState(state, action):
+        # return state
+        # action is tuple (y, x, player)
+        # player is 1 or -1
+        newState = state.copy()
+        (y, x, player) = action
+        newState[y, x] = player
+        return newState
+
+    def getValidActions(state):
+        # return list of valid actions
+        xCount = 0
+        oCount = 0
+        blanks = []
+        for y, row in enumerate(state):
+            for x, entry in enumerate(row):
+                if entry == 1:
+                    xCount += 1
+                elif entry == -1:
+                    oCount += 1
+                else:
+                    blanks.append((y, x))
+        player = 1 if xCount == oCount else -1
+        return [(y, x, player) for (y, x) in blanks]
+
+    def gameEnded(state):
+        # return boolean of whether game is ended
+        blankCount = sum(row.count(0) for row in state)
+        if blankCount == 0:
+            return True
+        
+        def triple(row):
+            # returns if a row is 3 Xs or 3 Os
+            return 3 in [row.count(1), row.count(-1)]
+        for row in state:
+            if triple(row):
+                return True
+        for col in state.transpose():
+            if triple(col):
+                return True
+        if triple(state.diagonal()) or triple(state.fliplr().diagonal()):
+            return True
+        return False
+
+    def gameReward(state):
+        # returns -1 if win for current player in this state, 1 if loss, 0 if tie
+        # note that win at the start of your turn is impossible
+        xCount = 0
+        oCount = 0
+        for y, row in enumerate(state):
+            for x, entry in enumerate(row):
+                if entry == 1:
+                    xCount += 1
+                elif entry == -1:
+                    oCount += 1
+        player = 1 if xCount == oCount else -1
+        
+        def lose(row):
+            # returns if a row is 3 in a row
+            return row.count(-player) == 3
+        
+        for row in state:
+            if lose(row):
+                return 1
+        for col in state.transpose():
+            if lose(col):
+                return 1
+        if lose(state.diagonal()) or lose(state.fliplr().diagonal()):
+            return 1
+        return 0 # If the player doesn't win, it must be a draw!
---
+++ mcts.py
@@ -0,0 +1,62 @@
+# Taken from https://web.stanford.edu/~surag/posts/alphazero.html
+# Taken from https://github.com/BryceGerst/PythonChessBot
+
+import numpy as np
+from math import sqrt
+
+class MCTS:
+    # We have Q[s][a], which returns the expected reward for making a move whose string representation
+    # is a from the game board whose hash value is s.
+    # We have N[s][a], which returns the number of times that we have explored making a move whose string
+    # representation is a from the game board whose hash value is s.
+    # Finally, we have P[s][a], which returns the initial probablility of making a move whose string
+    # representation is a from the game board whose hash value is s. (aka the policy)
+    # These will all be stored as dictionaries (hash tables) with the board hash as a key and an array
+    # of size 4672 as values.
+    def __init__(self):
+        self.Q = {}
+        self.N = {}
+        self.P = {}
+        self.visited = set()
+        # visited and P should have the same number of elements in them
+
+        # c_puct "is a hyperparameter that controls the degree of exploration"
+        # hyperparameter means that it is external to the model itself
+        self.c_puct = 0.2
+
+    def search(self, s, game, nnet):
+        if game.gameEnded(s): return -game.gameReward(s)
+
+        s_key = str(s) # get hash from state to use as dictionary key
+
+        if s_key not in self.visited:
+            self.visited.add(s_key)
+            self.P[s_key], v = nnet(s)
+            self.Q[s_key] = np.zeros((3,3,2))
+            self.N[s_key] = np.zeros((3,3,2))
+            return -v
+      
+        max_u, best_a = -float("inf"), -1
+        for a in game.getValidActions(s):
+            a_key = (a[0], a[1], 0 if a[2] == 1 else 1) # Convert action to np index
+            u = self.Q[s_key][a_key] + self.c_puct*self.P[s_key][a_key]*sqrt(sum(self.N[s_key]))/(1+self.N[s_key][a_key])
+            if u > max_u:
+                max_u = u
+                best_a = a
+        a = best_a
+        a_key = (a[0], a[1], 0 if a[2] == 1 else 1) # Convert action to np index
+        
+        sp = game.nextState(s, a)
+        v = search(sp, game, nnet)
+
+        self.Q[s_key][a_key] = (self.N[s_key][a_key]*self.Q[s_key][a_key] + v)/(self.N[s_key][a_key]+1)
+        self.N[s_key][a_key] += 1
+        return -v
+
+    def pi(self, s):
+        if s in self.P:
+            return self.P[s]
+        else:
+            print('error, gamestate not found')
+            return None
+
