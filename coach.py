eeb4186e5da5cef0207a3206d1bb66b5c894548a
StardustGogeta <stardustgogeta0@gmail.com>
2021-03-07 23:15:49 -0500

    Fixing some issues, making more progress

--- coach.py
+++ coach.py
@@ -26,7 +26,7 @@ def executeEpisode(game, nnet):
     mcts = MCTS()
     
     while True:
-        a, examples = getOptimalAction(game, mcts, nnet, examples)
+        a, examples = getOptimalAction(game, s, mcts, nnet, examples)
         s = game.nextState(s,a)
         if game.gameEnded(s):
             examples = assignRewards(examples, game.gameReward(s)) 
@@ -38,7 +38,7 @@ def assignRewards(examples, reward):
         reward = -reward
     return examples
 
-def getOptimalAction(game, mcts, nnet, examples=None):
+def getOptimalAction(game, s, mcts, nnet, examples=None):
     for _ in range(numMCTSSims):
         mcts.search(s, game, nnet)
     policy = mcts.pi(s)
@@ -89,33 +89,91 @@ def pit(new, old, game, nnet):
             if game.gameEnded(s):
                 newWins += (game.gameReward(s) + 1) / 2
                 break # end the game simulation
-     return newWins / gameCount
+
+    return newWins / gameCount
 
 def initNNet():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(3,3)),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(5)
-    ])
+    # creates and returns new neural network
+
+    # standard sequential model:
+##    model = tf.keras.models.Sequential([
+##        tf.keras.layers.Flatten(input_shape=(3,3)),
+##        tf.keras.layers.Dense(64, activation='relu'),
+##        tf.keras.layers.Dropout(0.2),
+##        tf.keras.layers.Dense(5)
+##    ])
+
+    # functional model:
+##    inputs = tf.keras.Input(shape=(3,3))
+##    x = tf.keras.layers.Dense(4, activation=tf.nn.relu)(inputs)
+##    outputs = tf.keras.layers.Dense(5, activation=tf.nn.softmax)(x)
+##    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+    # modified functional:
+    # https://www.pyimagesearch.com/2019/02/04/keras-multiple-inputs-and-mixed-data/
+    Input = tf.keras.Input
+    Dense = tf.keras.layers.Dense
+    Flatten = tf.keras.layers.Flatten
+    Concatenate = tf.keras.layers.Concatenate
+    Lambda = tf.keras.layers.Lambda
+    Model = tf.keras.Model
+    
+##    boardInput = Input(shape=(3,3))
+##    policyInput = Input(shape=(3,3,2))
+##
+##    x = Dense(64, activation='relu')(boardInput)
+##    x = Dense(2, activation='relu')(x)
+##    x = Model(inputs=boardInput, outputs=x)
+##
+##    y = Dense(64, activation='relu')(policyInput)
+##    y = Dense(2, activation='relu')(y)
+##    y = Model(inputs=policyInput, outputs=y)
+##
+##    combined = Concatenate([x.output, y.output])
+
+
+    # Goes like this:
+    # x (board) --| --> y (policy)
+    #             | --> z (value)
+    #
+    boardInput = Input(shape=(3,3))
+    #print(boardInput.shape)
+    x = Dense(64)(boardInput)
+    x = Dense(20, activation='relu')(x)
+    x = Flatten()(x)
+
+    y = Dense(18, name="dense_policy_1")(x)
+    #y.reshape((3,3,2))
+    #y = Model(inputs=boardInput, outputs=y)
+
+    z = Dense(10, name="dense_value_1")(x)
+    z = Dense(1, name="dense_value_2")(z)
+    #z = Model(inputs=boardInput, outputs=z)
+
+    #print(y.summary())
+    #print(z.summary())
+
+    a = Concatenate()([y, z])
+
+    #output = Lambda(lambda t: Concatenate()(t))([y, z])
+    
+    model = Model(inputs=boardInput, outputs=a)
+    print(model.summary())
+
+    # when we train it, we want to feed in examples of good moves and bad moves
+    # the model should be able to take in a board state and return an optimal policy, and if it is winning or losing
+    
     loss_fn = tf.keras.losses.MeanSquaredError()
     model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
     return model
 
 def trainNNet(nnet, examples):
-    # examples are (state, policy, value)
-    x_train, _, y_train = zip(*examples)
-    model.fit(
+    # examples are of the form (state, policy, value)
+    x_train, p_train, y_train = zip(*examples)
+    model.fit([x_train, p_train], y_train, epochs=5, verbose=1)
+
+game = Game()
+policyIterSP(game)
 
 
 
-
-
-
-
-
-
-
-
-
-    
